01d1cf9a8ba454ed3d4f41edd700e0712e64ce81
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-12-06 22:15:12 +0600

    Navbar and banner setup

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,112 +1,89 @@
 import { useContext, useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
-import { Link } from 'react-router-dom';
-import Swal from "sweetalert2";
+import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../provider/AuthProvider";
+import Swal from "sweetalert2";
+import Headroom from "react-headroom";
 
 const Header = () => {
-    const { user, logOut } = useContext(AuthContext)
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const { user, logOutUser } = useContext(AuthContext)
     const [isOpen, setIsOpen] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
     // Sign Out
     const handleLogout = () => {
-        logOut()
+        logOutUser()
             .then(() => Swal.fire('Oops!', 'LogOut Successful!', 'success'))
             .catch((err) => Swal.fire('Oops!', err.message, 'error'))
     }
 
     const navLink = <>
         <ul className="menu menu-horizontal px-1 lg:flex gap-1 hidden">
-            <li>
-                <NavLink to={'/'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#a433aa] hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}>
-                    Home
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to={'/availableFoods'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#a433aa] hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}                        >
-                    Available Foods
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to={'/addFood'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#a433aa] hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}                        >
-                    Add Food
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to={'/manageFoods'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#a433aa] hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}                        >
-                    Manage Foods
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to={'/foodRequest'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#a433aa] hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}                        >
-                    Food Request
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to={'/signUp'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#a433aa] hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}>
-                    Sign Up
-                </NavLink>
-            </li>
+
+            <NavLink
+                to={'/'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold" : "btn bg-[#616d7b81] hover:bg-[#2d3434] text-white text-[14px] font-semibold"}>
+                Home
+            </NavLink>
+
+
+            <NavLink to={'/availableFoods'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold border-none" : "btn bg-[#616d7b81] border-none hover:bg-[#2d3434] text-white text-[14px] font-semibold"}                       >
+                Available Foods
+            </NavLink>
+
+            <NavLink to={'/addFood'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold border-none" : "btn bg-[#616d7b81] border-none hover:bg-[#2d3434] text-white text-[14px] font-semibold"}                      >
+                Add Food
+            </NavLink>
+
+            <NavLink to={'/manageFoods'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold border-none" : "btn bg-[#616d7b81] border-none hover:bg-[#2d3434] text-white text-[14px] font-semibold"}                      >
+                Manage Foods
+            </NavLink>
+
+            <NavLink to={'/foodRequest'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold border-none" : "btn bg-[#616d7b81] border-none hover:bg-[#2d3434] text-white text-[14px] font-semibold"}                      >
+                Food Request
+            </NavLink>
+
+            <NavLink to={'/signUp'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold border-none" : "btn bg-[#616d7b81] border-none hover:bg-[#2d3434] text-white text-[14px] font-semibold"}>
+                Sign Up
+            </NavLink>
         </ul>
     </>
 
     const navLinkMd = <>
-        <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 bg-slate-400 text-white">
-            <li>
-                <NavLink to={'/'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#a433aa] my-3 hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}>
-                    Home
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to={'/availableFoods'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#a433aa] my-3 hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}                        >
-                    Available Foods
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to={'/addFood'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#a433aa] hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}                        >
-                    Add Food
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to={'/manageFoods'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#a433aa] hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}                        >
-                    Manage Foods
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to={'/foodRequest'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#a433aa] hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}                        >
-                    Food Request
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to={'/signUp'}
-                    className={({ isActive, isPending }) =>
-                        isPending ? 'pending' : isActive ? 'bg-[#753278] hover:text-blue-400 font-bold py-2 px-5 rounded-md text-white' : ''}>
-                    Sign Up
-                </NavLink>
-            </li>
+        <ul className="menu flex flex-col gap-2 menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 bg-slate-400 text-white">
+            <NavLink to={'/'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold border-none" : "btn bg-[#616d7b81] border-none hover:bg-[#2d3434] text-white text-[14px] font-semibold"}>
+                Home
+            </NavLink>
+
+            <NavLink to={'/availableFoods'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold border-none" : "btn bg-[#616d7b81] border-none hover:bg-[#2d3434] text-white text-[14px] font-semibold"}                        >
+                Available Foods
+            </NavLink>
+
+            <NavLink to={'/addFood'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold border-none" : "btn bg-[#616d7b81] border-none hover:bg-[#2d3434] text-white text-[14px] font-semibold"}                      >
+                Add Food
+            </NavLink>
+
+            <NavLink to={'/manageFoods'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold border-none" : "btn bg-[#616d7b81] border-none hover:bg-[#2d3434] text-white text-[14px] font-semibold"}                      >
+                Manage Foods
+            </NavLink>
+
+            <NavLink to={'/foodRequest'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold border-none" : "btn bg-[#616d7b81] border-none hover:bg-[#2d3434] text-white text-[14px] font-semibold"}                      >
+                Food Request
+            </NavLink>
+
+            <NavLink to={'/signUp'}
+                className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold border-none" : "btn bg-[#616d7b81] border-none hover:bg-[#2d3434] text-white text-[14px] font-semibold"}>
+                Sign Up
+            </NavLink>
         </ul>
     </>
 
@@ -132,59 +109,74 @@ const Header = () => {
     };
 
     return (
-        <div className="max-w-7xl mx-auto sticky top-0 z-50">
-            <div className="navbar flex justify-between">
-                <div className="lg:navbar-start lg:w-auto w-3/2">
-                    <img className="h-[50px] w-[50] hidden lg:block rounded-full" src="https://img.freepik.com/premium-photo/tandoori-chicken-is-chicken-dish-prepared-by-roasting-chicken-marinated-yogurt-spices-tandoor-clay-oven-served-with-onion-green-chutney_466689-77615.jpg?w=996" alt="" />
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-9 mr-2 text-white bg-slate-600 font-black w-9 rounded-lg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        {navLinkMd}
-                    </div>
-                    <Link to='/'><button className="normal-case text-[22px] md:text-3xl font-bold text-[#6531ae]">Food Sharing</button></Link>
-                </div>
-                <div className="navbar-center lg:mx-auto lg:w-auto bg-white rounded-lg lg:flex">
-                    {navLink}
-                </div>
-                <div className="lg:navbar-end lg:w-auto w-3/1">
-                    {/* Dark Mode button */}
-                    <input
-                        type="checkbox"
-                        className="toggle mx-2"
-                        checked={isDarkMode}
-                        onChange={handleToggle}
-                    />
-
-                    <div
-                        onClick={() => setIsOpen(!isOpen)}
-                    >
-                        {user ? <>
-                            <label tabIndex={0} className="btn btn-secondary btn-circle avatar">
-                                <div className="w-10 rounded-full">
-                                    <label>
-                                        <div>
-                                            <img className="w-10 rounded-full" src={user?.photoURL} />
+        <Headroom className="absolute top-0 right-0 left-0">
+            <div className="bg-[#8e8e94c3]">
+                <div className="max-w-7xl mx-auto">
+                    <div className="navbar p-0 px-4 flex justify-between">
+                        <div className="lg:navbar-start lg:w-auto w-3/2">
+                            <Link to='/'>
+                                <div className="flex items-center">
+                                    <div className="flex items-center">
+                                        <div className="avatar mr-1">
+                                            <div className="md:w-12 w-10 rounded-full">
+                                                <img src="https://img.freepik.com/free-photo/delicious-burger-with-many-ingredients-isolated-white-background-tasty-cheeseburger-splash-sauce_90220-1266.jpg?w=740&t=st=1699466113~exp=1699466713~hmac=3607eaccb86812f98538d14a38e39af830f742b407a085eb3e1306b074ea2fb9" />
+                                            </div>
                                         </div>
-                                    </label>
+                                        <div className="dropdown">
+                                            <label tabIndex={0} className="btn-ghost lg:hidden">
+                                                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="22" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
+                                            </label>
+                                            {navLinkMd}
+                                        </div>
+                                    </div>
+                                    <button className="normal-case ml-2 text-[24px] md:text-3xl font-bold text-white">SurplusSaver</button>
                                 </div>
-                            </label>
-
-                        </>
-                            : <Link to={'/login'}><button className="btn btn-primary mr-3">Login</button></Link>}
-                    </div>
-                    {user && isOpen && (
-                        <div className='absolute rounded-xl shadow-md w-[40vw] p-2 md:p-2 lg:w-[10vw] mr-2 md:w-[20vw] bg-white right-0 top-20'>
-                            <div className='flex flex-col justify-center items-center cursor-pointer'>
-                                <h3 className="font-bold text-center text-green-500">{user?.displayName}</h3>
-                                <button onClick={handleLogout} className="lg:px-5 px-3 mt-2 text-white lg:mt-3 rounded-lg text-[16px] font-bold py-3 bg-[#a433aa]">Login Out</button>
-                            </div>
+                            </Link>
                         </div>
-                    )}
+                        <div className="navbar-center lg:mx-auto lg:w-auto rounded-lg lg:flex">
+                            {navLink}
+                        </div>
+                        <div className="lg:navbar-end lg:w-auto w-3/1">
+                            <div onClick={() => setIsOpen(!isOpen)}>
+                                {
+                                    user ? <>
+                                        <label tabIndex={0} className="btn btn-primary btn-circle avatar">
+                                            <div className="w-10 rounded-full">
+                                                <label>
+                                                    <div>
+                                                        <img className="w-10 rounded-full cursor-pointer" src={user?.photoURL} />
+                                                    </div>
+                                                </label>
+                                            </div>
+                                        </label>
 
+                                    </>
+                                        : <NavLink to={'/login'}
+                                            className={({ isPending, isActive }) => isPending ? "bg-[#92a1eb] hover:bg-[#92a1eb] text-white btn text-[14px] font-semibold" : isActive ? "btn bg-[#fff] text-[14px] font-semibold" : "btn bg-[#616d7b81] text-white text-[14px] font-semibold"}                      >
+                                            Sign In
+                                        </NavLink>
+                                }
+                            </div>
+                            {user && isOpen && (
+                                <div className='absolute rounded-xl shadow-md w-[40vw] p-2 md:p-2 lg:w-[10vw] mr-2 md:w-[20vw] bg-[#514e9ab8] right-0 top-20'>
+                                    <div className='flex flex-col py-2 justify-center items-center'>
+                                        <h3 className="font-bold text-center text-white disabled">{user?.displayName}</h3>
+                                        {/* Dark Mode button */}
+                                        <input
+                                            type="checkbox"
+                                            className="toggle mt-2"
+                                            checked={isDarkMode}
+                                            onChange={handleToggle}
+                                        />
+                                        <button onClick={handleLogout} className="lg:px-5 px-3 mt-2 hover:bg-gray-500 text-white lg:mt-3 rounded-lg text-[14px] font-bold py-3  bg-violet-400">Login Out</button>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Headroom>
     );
 };
 
--- src/components/pages/home/home/Home.jsx
+++ src/components/pages/home/home/Home.jsx
@@ -11,21 +11,18 @@ const Home = () => {
         Aos.init({ duration: 2000 });
     }, [])
 
-    const containerStyle = {
-        backgroundImage: 'url("https://img.freepik.com/free-photo/exploding-burger-with-vegetables-melted-cheese-black-background-generative-ai_157027-1734.jpg?w=1060&t=st=1699117652~exp=1699118252~hmac=de378d71376c9161300c2a060165e9595926c34ddebc57c4115e1357fc1901c7")',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'top',
-        width: "100%",
-    };
-
     return (
         <>
             <Helmet>
-                <title>Food Sh | Home</title>
+                <title>SurplusSaver | Home</title>
             </Helmet>
-            <div style={containerStyle} data-aos="zoom-in">
-                <div className="flex items-center justify-center h-[91vh] px-8 lg:px-0">
+            <div className="hero min-h-screen" data-aos="zoom-in" style={{
+                backgroundImage: 'url(https://img.freepik.com/free-photo/exploding-burger-with-vegetables-melted-cheese-black-background-generative-ai_157027-1734.jpg?w=1060&t=st=1699117652~exp=1699118252~hmac=de378d71376c9161300c2a060165e9595926c34ddebc57c4115e1357fc1901c7)',
+                objectFit: "cover",
+                backgroundPosition: "top"
+            }}>
+                <div className="hero-overlay bg-opacity-20"></div>
+                <div className="hero-content text-center text-neutral-content">
                     <h1 className="lg:text-5xl text-3xl text-white font-bold text-center">Community Food Sharing <br /> and Surplus
                         Reduction Platform!!!</h1>
                 </div>
