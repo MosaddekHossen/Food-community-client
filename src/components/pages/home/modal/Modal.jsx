4cab401326e5c934b5ce5803a08e97628f33891d
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-12-07 17:23:26 +0600

    Food request created

--- src/components/pages/home/modal/Modal.jsx
+++ src/components/pages/home/modal/Modal.jsx
@@ -4,23 +4,18 @@ import { AuthContext } from '../../../../provider/AuthProvider';
 import Swal from 'sweetalert2';
 
 const Modal = ({ food }) => {
-    // const { foodName, foodQuantity, pickupLocation, expiredDate, additionalNotes, foodStatus, userName, userImage, foodImage } = food || {};
+    const { foodName, foodQuantity, pickupLocation, expiredDate, userName, foodImage } = food || {};
     const { user } = useContext(AuthContext);
-    const { _id, foodName, pickupLocation, expiredDate, foodImage } = food || {};
-    const { displayName, email } = user || {};
+    const { email } = user || {};
 
     const handleRequest = event => {
         event.preventDefault();
         const form = event.target;
-        const foodName = form.foodName.value;
-        const foodQuantity = form.foodQuantity.value;
-        const pickupLocation = form.pickupLocation.value;
-        const expiredDate = form.expiredDate.value;
         const additionalNotes = form.additionalNotes.value;
         const foodStatus = form.foodStatus.value;
-        const userName = form.userName.value;
         const userEmail = form.userEmail.value;
         const foodImage = form.foodImage.value;
+
         const AddFood = { foodName, foodQuantity, pickupLocation, expiredDate, additionalNotes, foodStatus, userName, userEmail, foodImage }
 
         // Send data to the server
@@ -58,51 +53,17 @@ const Modal = ({ food }) => {
                     <div className="md:flex gap-10 justify-between md:mb-5">
                         <div className="form-control md:w-1/2">
                             <label className="label">
-                                <span className="label-text">Food Name</span>
+                                <span className="label-text">Food Image</span>
                             </label>
-                            <label>
-                                <input type="text" readOnly required defaultValue={foodName} name="foodName" placeholder="Food Name" className="input input-bordered w-full" />
-                            </label>
+                            <img className='w-full h-[200px] object-cover' name="foodImage" defaultValue={foodImage} src={foodImage} alt="Food Image" />
                         </div>
                         <div className="form-control md:w-1/2">
                             <label className="label">
-                                <span className="label-text">Food Id</span>
+                                <span className="label-text">User Email</span>
                             </label>
                             <label>
-                                <input type="text" required readOnly defaultValue={_id} name="foodQuantity" placeholder="Food Quantity" className="input input-bordered w-full" />
+                                <input type="email" required name="userEmail" placeholder="User Email" readOnly defaultValue={email} className="input input-bordered w-full" />
                             </label>
-                        </div>
-                    </div>
-                    {/* Form row */}
-                    <div className="md:flex gap-10 justify-between md:mb-5">
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">Pickup Location</span>
-                            </label>
-                            <label>
-                                <input type="text" readOnly required defaultValue={pickupLocation} name="pickupLocation" placeholder="Pickup Location" className="input input-bordered w-full" />
-                            </label>
-                        </div>
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">Expired Date</span>
-                            </label>
-                            <label>
-                                <input type="date" required readOnly defaultValue={expiredDate} name="expiredDate" placeholder="Expired Date" className="input input-bordered w-full" />
-                            </label>
-                        </div>
-                    </div>
-                    {/* Form row */}
-                    <div className="md:flex gap-10 justify-between md:mb-5">
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">Additional Notes</span>
-                            </label>
-                            <label>
-                                <input type="text" required name="additionalNotes" placeholder="Additional Notes" className="input input-bordered w-full" />
-                            </label>
-                        </div>
-                        <div className="form-control md:w-1/2">
                             <label className="label">
                                 <span className="label-text">Donation Money</span>
                             </label>
@@ -113,31 +74,12 @@ const Modal = ({ food }) => {
                     </div>
                     {/* Form row */}
                     <div className="md:flex gap-10 justify-between md:mb-5">
-                        <div className="form-control md:w-1/2">
+                        <div className="form-control md:w-full">
                             <label className="label">
-                                <span className="label-text">Food Donator Name</span>
+                                <span className="label-text">Notes</span>
                             </label>
-                            <label>
-                                <input required type="text" name="userName" placeholder="User Name" readOnly defaultValue={displayName} className="input input-bordered w-full" />
-                            </label>
+                            <textarea name="additionalNotes" className='border-slate-500 p-3 mb-5 border' id="2" required placeholder="Additional Notes" cols="10" rows="5"></textarea>
                         </div>
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">User Email</span>
-                            </label>
-                            <label>
-                                <input type="email" required name="userEmail" placeholder="User Email" readOnly defaultValue={email} className="input input-bordered w-full" />
-                            </label>
-                        </div>
-                    </div>
-                    {/* Form row */}
-                    <div className="form-control md:w-full md:mb-10 pb-5">
-                        <label className="label">
-                            <span className="label-text">Food Image</span>
-                        </label>
-                        <label>
-                            <input type="text" readOnly required defaultValue={foodImage} name="foodImage" placeholder="Food Image" className="input input-bordered w-full" />
-                        </label>
                     </div>
                     <input type="submit" className="btn btn-block bg-gray-600 text-white hover:text-blue-600 font-bold" value="Request Submit" />
                 </form>
